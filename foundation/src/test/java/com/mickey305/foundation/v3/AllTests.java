37f985ceb4a1c506511ff6b8997e523d479a254f
K.Misaki <pro.mickey.12091it@gmail.com>
2018-12-01 17:17:32 +0900

    依存ライブラリのバージョンアップデート

--- foundation/src/test/java/com/mickey305/foundation/v3/AllTests.java
+++ foundation/src/test/java/com/mickey305/foundation/v3/AllTests.java
@@ -1,25 +1,26 @@
-package com.mickey305.foundation.v3;
-
-import com.mickey305.foundation.v3.ansi.code.AnsiStringBuilderTest;
-import com.mickey305.foundation.v3.lang.tuple.MutablePairTest;
-import com.mickey305.foundation.v3.util.ListUtilTest;
-import com.mickey305.foundation.v3.util.concurrent.ContainerTest;
-import com.mickey305.foundation.v3.util.pattern.ComponentTest;
-import com.mickey305.foundation.v3.util.pattern.CompositeTest;
-import com.mickey305.foundation.v3.util.pattern.LeafTest;
-import com.mickey305.foundation.v3.validation.ValidationInvokerTest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        AnsiStringBuilderTest.class,
-        MutablePairTest.class,
-        ContainerTest.class,
-        ComponentTest.class,
-        CompositeTest.class,
-        LeafTest.class,
-        ListUtilTest.class,
-        ValidationInvokerTest.class
-})
-public class AllTests {}
+//package com.mickey305.foundation.v3;
+//
+//import com.mickey305.foundation.v3.ansi.code.AnsiStringBuilderTest;
+//import com.mickey305.foundation.v3.lang.tuple.MutablePairTest;
+//import com.mickey305.foundation.v3.util.ListUtilTest;
+//import com.mickey305.foundation.v3.util.concurrent.ContainerTest;
+//import com.mickey305.foundation.v3.util.pattern.ComponentTest;
+//import com.mickey305.foundation.v3.util.pattern.CompositeTest;
+//import com.mickey305.foundation.v3.util.pattern.LeafTest;
+//import com.mickey305.foundation.v3.validation.ValidationInvokerTest;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//        AnsiStringBuilderTest.class,
+//        MutablePairTest.class,
+//        ContainerTest.class,
+//        ComponentTest.class,
+//        CompositeTest.class,
+//        LeafTest.class,
+//        ListUtilTest.class,
+//        ValidationInvokerTest.class
+//})
+//
+//public class AllTests {}
