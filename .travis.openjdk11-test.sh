97af97edfb54f9ef7a8c4f33083fa19b2aaf58d6
K.Misaki <pro.mickey.12091it@gmail.com>
2018-12-04 01:59:17 +0900

    ビルドスクリプトの更新

---
+++ .travis.openjdk11-test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "実行するには1個の引数が必要です。" 1>&2
+  exit 1
+fi
+
+COMPILE_JDK=$1
+
+# compile
+jdk_switcher use ${COMPILE_JDK}
+echo ${JAVA_HOME} ; javac -version ; java -version
+./gradlew clean compileJava compileTestJava --stacktrace --info
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
+
+# test
+export _JAVA_OPTIONS=
+mkdir -p ${HOME}/tmp
+export JAVA_HOME=`./install-jdk.sh -F 11 -W ${HOME}/tmp -E | tail -n 1`
+export PATH=${JAVA_HOME}/bin:$PATH
+echo ${JAVA_HOME} ; javac -version ; java -version
+./gradlew check -x compileJava --stacktrace --info
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
+
+exit 0
