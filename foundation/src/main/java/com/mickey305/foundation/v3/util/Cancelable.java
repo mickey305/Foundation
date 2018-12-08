4b1c32ab193af78b4281da05a2ec57fbe8a7a7d2
K.Misaki <pro.mickey.12091it@gmail.com>
2018-12-08 23:42:55 +0900

    Javadoc関連のエラーを修正

--- foundation/src/main/java/com/mickey305/foundation/v3/util/Cancelable.java
+++ foundation/src/main/java/com/mickey305/foundation/v3/util/Cancelable.java
@@ -3,7 +3,7 @@ package com.mickey305.foundation.v3.util;
 public interface Cancelable<R> {
   /**
    * 取り消し内容
-   * <p>コマンドの実行内容{@link Executable<R>}に対する取り消し内容を実装する</p>
+   * <p>コマンドの実行内容{@link Executable}に対する取り消し内容を実装する</p>
    *
    * @return 処理結果
    */
