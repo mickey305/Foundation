aa196c1fa70d21e995e7b56b1482a47caf272de6
K.Misaki <pro.mickey.12091it@gmail.com>
2018-12-25 00:59:07 +0900

    タプルクラスの実装誤りを修正

--- foundation/src/main/java/com/mickey305/foundation/v3/lang/tuple/ImmutableSingle.java
+++ foundation/src/main/java/com/mickey305/foundation/v3/lang/tuple/ImmutableSingle.java
@@ -1,12 +1,10 @@
 package com.mickey305.foundation.v3.lang.tuple;
 
-import javax.annotation.Nonnull;
-
 public class ImmutableSingle<T> extends Single<T> {
   /**
    * use serialVersionUID for interoperability
    */
-  private static final long serialVersionUID = 1389534397433311780L;
+  private static final long serialVersionUID = 1662786483949947910L;
 
   public ImmutableSingle(T object) {
     super(object);
@@ -14,13 +12,5 @@ public class ImmutableSingle<T> extends Single<T> {
 
   public static <T> ImmutableSingle<T> of(T object) {
     return new ImmutableSingle<>(object);
-  }
-
-  public void setNull() {
-    super.setObject(null);
-  }
-
-  public void set(@Nonnull T object) {
-    super.setObject(object);
   }
 }
--- foundation/src/main/java/com/mickey305/foundation/v3/lang/tuple/MutableSingle.java
+++ foundation/src/main/java/com/mickey305/foundation/v3/lang/tuple/MutableSingle.java
@@ -1,10 +1,12 @@
 package com.mickey305.foundation.v3.lang.tuple;
 
+import javax.annotation.Nonnull;
+
 public class MutableSingle<T> extends Single<T> {
   /**
    * use serialVersionUID for interoperability
    */
-  private static final long serialVersionUID = 1662786483949947910L;
+  private static final long serialVersionUID = 1389534397433311780L;
 
   public MutableSingle(T object) {
     super(object);
@@ -12,5 +14,13 @@ public class MutableSingle<T> extends Single<T> {
 
   public static <T> MutableSingle<T> of(T object) {
     return new MutableSingle<>(object);
+  }
+
+  public void setNull() {
+    super.setObject(null);
+  }
+
+  public void set(@Nonnull T object) {
+    super.setObject(object);
   }
 }
