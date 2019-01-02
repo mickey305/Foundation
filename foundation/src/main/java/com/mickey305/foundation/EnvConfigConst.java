a02134eae10497c77eb6e247fcf605f4db035ef5
K.Misaki <pro.mickey.12091it@gmail.com>
2019-01-02 17:32:13 +0900

    ライブラリ環境を変更

--- foundation/src/main/java/com/mickey305/foundation/EnvConfigConst.java
+++ foundation/src/main/java/com/mickey305/foundation/EnvConfigConst.java
@@ -21,8 +21,8 @@ public final class EnvConfigConst {
   /*
    * Build type mask
    */
-  private static final boolean IS_PRODUCTION_MODE = false;
-  private static final boolean IS_DEVELOP_MODE = true;
+  private static final boolean IS_PRODUCTION_MODE = true;
+  private static final boolean IS_DEVELOP_MODE = false;
   private static final boolean IS_TEST_MODE = false;
   
   /**
