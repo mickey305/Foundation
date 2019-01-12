f08d58aeb9931689b82dc4bdc4eb371dcedae6d2
K.Misaki <pro.mickey.12091it@gmail.com>
2019-01-13 07:20:16 +0900

    ロック関連クラスを追加

---
+++ foundation/src/main/java/com/mickey305/foundation/v3/util/concurrent/AutoUnlock.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019. K.Misaki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.mickey305.foundation.v3.util.concurrent;
+
+import com.mickey305.foundation.v3.util.Log;
+
+import static com.mickey305.foundation.EnvConfigConst.IS_DEBUG_MODE;
+
+public abstract class AutoUnlock implements AutoCloseable {
+  private final LockType type;
+  private final ILockable<LockType> lockable;
+  
+  public static final class ReadLock extends AutoUnlock {
+    public ReadLock(ILockable<LockType> lockable) { super(LockType.Read, lockable); }
+  }
+  
+  public static final class WriteLock extends AutoUnlock {
+    public WriteLock(ILockable<LockType> lockable) { super(LockType.Write, lockable); }
+  }
+  
+  protected AutoUnlock(LockType type, ILockable<LockType> lockable) {
+    this.lockable = lockable;
+    this.type = type;
+    
+    // lock start
+    this.lockable.lock(this.type);
+    
+    if (IS_DEBUG_MODE) {
+      Log.d(((type == LockType.Read)? "[R]" : "[W]") + " locked");
+    }
+  }
+  
+  @Override
+  public void close() throws Exception {
+    // lock end
+    lockable.unlock(type);
+  
+    if (IS_DEBUG_MODE) {
+      Log.d(((type == LockType.Read)? "[R]" : "[W]") + " unlocked");
+    }
+  }
+}
